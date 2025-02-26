adf614d644bef4810d79fb48ccdc759bea887c3e
baleradaniel <danielbalera021@gmail.com>
2025-02-25 21:03:21 -0300

    Add files via upload

---
+++ danielbalera_db.sql
@@ -0,0 +1,12 @@
+create database danielbalera_db
+
+use danielbalera_db
+
+create table usuario(
+	id_usuario int not null auto_increment,
+    nome text,
+    telefone text,
+    usuario text,
+    senha text,
+    primary key (id_usuario)
+);
